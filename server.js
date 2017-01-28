d5867aeb81c7ddebaa82a78b9f4ad5e24047efd3
jhuan102 <300818592@cencol.ca>
2017-01-28 14:41:50 -0500

    Using res.send to save us lines of codes

--- server.js
+++ server.js
@@ -18,18 +18,12 @@ console.log(`Server started at http://localhost:${port}`);
 
 // second route is '/hello'
 app.use('/hello', (req, res, next) => {
-  res.setHeader('Content-Type', 'text/plain');
-  res.end("Hello, World!");
-
-  next();
+  res.send("Hello, World!");
 });
 
 // first route is '/' - root of my website
 app.use('/', (req, res, next) => {
-  res.setHeader('Content-Type', 'text/plain');
-  res.end("Welcome!");
-
-  next();
+  res.send("Welcome!");
 });
 
 
